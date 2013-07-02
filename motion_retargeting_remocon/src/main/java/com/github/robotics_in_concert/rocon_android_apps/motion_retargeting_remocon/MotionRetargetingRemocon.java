d1d37fd0eca51dbdff41df8f79dc894365b0ae69
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-07-02 10:00:30 +0900

    adds motion retargeting remote controller app

---
+++ motion_retargeting_remocon/src/main/java/com/github/robotics_in_concert/rocon_android_apps/motion_retargeting_remocon/MotionRetargetingRemocon.java
@@ -0,0 +1,15 @@
+package com.github.robotics_in_concert.rocon_android_apps.motion_retargeting_remocon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MotionRetargetingRemocon extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
